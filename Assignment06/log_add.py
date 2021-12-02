0ee93e618f062d521846c97e4d70706148e98b15
Lisa <63642930+CorvusCorax95@users.noreply.github.com>
2021-12-02 16:59:01 +0100

    final version with all outputs

--- Assignment06/log_add.py
+++ Assignment06/log_add.py
@@ -10,12 +10,16 @@ import sys
 import numpy as np
 
 def read_commandline():
+
     convarray = np.empty(0)
     fobj = open(sys.argv[1], 'r')
     for line in fobj:
         isValid = True
         iniarray = line.split()
         newarray = np.array(iniarray)
+        if(np.size(newarray) == 0):
+            print("Array is empty")
+            continue
         for x in newarray:
             test = x
             test = test.replace('.', '', 1)
@@ -24,24 +28,45 @@ def read_commandline():
             test = test.replace('inf', '1',1)
             test = test.replace('nan', '1', 1)
             test = test.replace('e', '1',1)
+            
             if not test.isdigit():           
                 isValid = False
-                print("Line not valid.")
-            
+                print("Line not valid")
+                break
+                
+        print("-----------------")
+        print("Original Array", line)
         if(isValid):
             convarray = newarray.astype(float)
+            print("Float Array:", convarray)
+                
             log_add(convarray)
-        print(convarray)
-  
+        else:
+            print("ERROR - FALSE INPUT")
+    fobj.close()
+
 def log_add(array: np.array) -> np.array:
     addition = 0
-    size = np.size(array)
-    log_array = math.log(array)
-    print(array)
-    print(log_array)
-        # add i on i+1 and safe 
-        #addition += p1 + log(1+(np.exp(q1-p1)))     
-    return 0
+    prime_max = np.nanmax(array)
+    print("max of array:", prime_max)
+    
+    max_value = math.exp(prime_max)
+    print("exp(max of array):", max_value)
+    if(max_value == 0):
+        print("Max Value is 0, so Addition is 0.")
+        return
+    # return if array is empty
+    if(np.size(array) == 0):
+        print("Array is empty")
+        return
+    
+    y = np.size(array)-1
+    addition = max_value
+    
+    for i in range(0,y):
+        addition += array[i] / max_value
+        ##print(addition)
+    print("Addition r:", addition)
 
 
 read_commandline()
