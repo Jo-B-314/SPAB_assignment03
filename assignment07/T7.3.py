1189790569ea6f768f017568da5b33b2cce7bd65
Jo-B-314 <s8jobech@stud.uni-saarland.de>
2021-12-14 12:23:27 +0100

    forgot to push final state of assignment07

--- assignment07/T7.3.py
+++ assignment07/T7.3.py
@@ -7,11 +7,13 @@ Created on Tue Dec  7 15:55:08 2021
 import math
 import random
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import chi2
+from scipy.stats import rv_continuous
 
 from itertools import product
 
-motif1 = 'ATCTGGAC'
-motif2 = 'ACACACAC'
+# SORRY this is not working as it should
 
 alphabet = [
     'A',
@@ -19,23 +21,16 @@ alphabet = [
     'G',
     'T']
 
-"""
-g = G/C content
-l = length of genome
-n = number of repeats
+
+def convert(s):
+    strin = "" 
+    for x in s:
+        strin += x 
+    return strin
 
 """
-def convert(s):
-  
-    # initialization of string to ""
-    new = ""
-  
-    # traverse in the string 
-    for x in s:
-        new += x 
-  
-    # return string 
-    return new
+N = number of repeats
+"""
 
 def motif(n,k,N):
     l = n+k-1
@@ -45,14 +40,15 @@ def motif(n,k,N):
     
     # array of N lists, each list index counter for k_tuple
     nmotifs = np.zeros(N, dtype=list)
+    delta = np.zeros(a, dtype=int)
+    delta_pdf = np.zeros(a, dtype=int)
     empty = np.zeros(a, dtype=np.uint32)
     for i in range(N):
         nmotifs[i] = empty
         
-    #k-tuples
+    # k-tuples
     comb = product(alphabet, repeat=k)
     k_tuples_list = list(comb)
-    
     k_tuples_strings = np.zeros(a, dtype='object')
     for i in range(a):
         stri = []
@@ -61,13 +57,43 @@ def motif(n,k,N):
         string = convert(stri)
         k_tuples_strings[i] = string
     
+    # count
     for i in range(N):
-        # N-mal dna sequenz erstellen und nach k-tuepln suchen
+        # N-times construct DNA, search for k-tuples
         genome = "".join(random.choices(alphabet, probs, k=l))
         for j in range(a):
             nmotifs[i][j] = genome.count(k_tuples_strings[j])
-        #nmotif1[i] = genome.count(motif1)
-        #nmotif2[i] = genome.count(motif2)
+     
+        
+    # delta = sum (observed-expected)^2/expected
+    expected = n/a
+    
+    for j in range(a):
+        sum = 0
+        for i in range(N) :
+            x = math.pow(nmotifs[i][j]-expected, 2) / expected
+            sum += x
+        delta[j] = sum
+    d_sum = 0
+    for j in range(a):
+        d_sum += delta[j]
+        
+    mean = d_sum // a
+    sigma = 0
+    for l in delta :
+        sigma += (l-mean)**2
+    sigma = math.sqrt(sigma/a)
+    for i in range(a):
+        delta_pdf[i] = (1.0 / (sigma * math.sqrt(2*math.pi))) * math.exp(-0.5*math.pow(((delta[i]-mean)/sigma),2))
+    
+    # chi square, source geeksforgeeks
+    numargs = chi2.numargs
+    [a] = [1] * numargs
+    rv = chi2(a)
+    
+    distribution = np.linspace(0, np.minimum(rv.dist.b, 1))
+  
+    plot = plt.plot(distribution, rv.pdf(distribution))
   
         
-motif(100, 2, 5)
+motif(1000000, 8, 100)
